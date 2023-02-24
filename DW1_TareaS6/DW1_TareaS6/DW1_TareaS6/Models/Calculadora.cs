fc689b92fea6774e39fc7f0b5128feef4da17547
Cristian Rivera <adbel.rivera@unitec.edu>
2023-02-23 19:15:55 -0600

    comit
    Tarea semana 6

---
+++ DW1_TareaS6/DW1_TareaS6/DW1_TareaS6/Models/Calculadora.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace DW1_TareaS6.Models
+{
+    public class Calculadora
+    {
+
+        public int numero1 { get; set; }
+        public int numero2 { get; set; }
+
+
+        public int calcularSuma() {
+
+            int total = numero1 + numero2;
+
+            return total;
+        }
+        public int calcularResta()
+        {
+
+
+            int total = numero1 - numero2;
+
+            return total;
+        }
+        public int calcularMultiplicacion()
+        {
+
+
+            int total = numero1 * numero2;
+
+            return total;
+        }
+        public int calcularDivision()
+        {
+
+
+            int total = numero1 / numero2;
+
+            return total;
+        }
+        public override string ToString()
+        {
+            return $"Hola la suma de: {numero1} + : {numero2} es igual a  {calcularSuma()}   ";
+        }
+
+
+    }
+}
---
+++ DW1_TareaS6/DW1_TareaS6/DW1_TareaS6/ViewModel/ViewModelPersona.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using DW1_TareaS6.Models;
+
+namespace DW1_TareaS6.ViewModel
+
+{
+    public class ViewModelCalculadora : INotifyPropertyChanged
+    {
+
+        public ViewModelCalculadora() {
+
+            crearCalculadora = new Command(() => {
+
+                Calculadora c = new Calculadora() {
+                    numero1 = this.numero1, 
+                    numero2 = this.numero2
+                };
+
+                Mensaje = p.ToString();
+
+
+
+            });
+
+
+        }
+
+        string mensaje;
+        public string Mensaje {
+
+            get => mensaje;
+            set {
+                mensaje = value;
+                var args = new PropertyChangedEventArgs(nameof(Mensaje));
+                PropertyChanged?.Invoke(this, args);
+            
+            }
+        
+        }
+
+
+        string numero1;
+
+        public string Numero1 {
+
+            get => numero1;
+            set {
+
+                numero1 = value;
+                var args = new PropertyChangedEventArgs(nameof(Numero1));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+            
+       }
+
+        string numero2;
+
+        public string Numero2
+        {
+
+            get => numero2;
+            set
+            {
+
+                numero2 = value;
+                var args = new PropertyChangedEventArgs(nameof(Numero2));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+
+        }
+
+
+
+        public Command crearCalculadora { get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
